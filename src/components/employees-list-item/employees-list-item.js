1edffc7da9816c5cdc5887e8d6e0885c1a7982ae
EremenkoVO <eremenko.v.o@yandex.ru>
2022-08-23 14:21:21 +0300

    l1

---
+++ src/components/employees-list-item/employees-list-item.js
@@ -0,0 +1,22 @@
+import './employees-list-item.css';
+
+const EmployeesListItem = () => {
+  return (
+    <li className="list-group-item d-flex justify-content-between">
+      <span className="list-group-item-label">Vasya Pupkin</span>
+      <input type="text" defaultValue="1000$" />
+      <div className="d-flex justify-content-center align-items-center">
+        <button type="button" className="btn-cookie btn-sm">
+          <i className="fas fa-cookie"></i>
+        </button>
+
+        <button type="button" className="btn-trash btn-sm">
+          <i className="fas fa-trash"></i>
+        </button>
+        <i className="fa-solid fa-star"></i>
+      </div>
+    </li>
+  );
+};
+
+export default EmployeesListItem;
